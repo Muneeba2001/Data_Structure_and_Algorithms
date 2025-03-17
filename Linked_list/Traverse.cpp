19dc4ecef3a77032eea7b7566825713f1de7aeb8
Muneeba2001 <muneebawaseem1006@gmail.com>
2025-03-17 19:37:17 +0500

    Linked list traverse

---
+++ Linked_list/Traverse.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+     
+    Node(int val){
+        data = val;
+        next = nullptr;
+    }
+};
+
+void traverseList(Node* head){
+    Node* current = head;
+   while(current!= nullptr){
+        cout<<current->data<<" ";
+        current = current->next;
+   }
+    cout<<endl;
+}
+int main() {
+   Node *head= new Node(10);
+   head->next = new Node(20);
+   head->next->next = new Node(30);
+   head->next->next->next= new Node(40);
+    traverseList(head);
+    return 0;
+}
