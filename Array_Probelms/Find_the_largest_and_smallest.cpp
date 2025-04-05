8780e1d8592bfaa21d93df0163c4200dd81a0ef3
Muneeba2001 <muneebawaseem1006@gmail.com>
2025-04-05 20:19:07 +0500

    smallest and largest number

---
+++ Array_Probelms/Find_the_largest_and_smallest.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[]={4,5,7,2,8,3,9};
+    int n = sizeof(arr)/ sizeof(arr[0]);
+    int max=arr[0];
+    int min= arr[0];
+    for(int i=1; i<n; i++){
+        if(arr[i]>max){
+          max = arr[i];
+        }
+        else if(arr[i]<min){
+            min = arr[i];
+        }
+    }
+    cout<<"The largest number is "<<max<<endl;
+    cout<<"The smallest number is "<<min;
+    return 0;
+}
